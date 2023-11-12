2fa58c54a8ac7cd7f45a231c82f93e273d0b47db
iycodes <iycodes@outlook.com>
2023-11-12 11:56:13 +0100

    init,fonts and images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitanaFont = Lusitana({ weight: "700", subsets: ["latin"] });
